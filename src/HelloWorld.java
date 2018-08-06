e459ef44d362c33786e9d23b529c7b78dd75d3e7
pearlsm20 <pearlsm24@gmail.com>
2018-08-06 18:32:25 +0900

    배고파 눕고싶다 눈이 막 감키고 있음

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
 			System.out.println("Helloworld~!"); 
 			System.out.println("ㅎ2ㅎ2ㅎ2ㅎ2ㅎ2ㅎ2ㅎ2ㅎ2");
 			System.out.println("졸려");
+			System.out.println("배고파");
+			System.out.println("눕고싶다");
+			System.out.println("눈이 막 감키고 있음");
 		}
 }
