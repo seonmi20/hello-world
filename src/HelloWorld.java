a8749eddd96203c40dfd3ba887e84f98db347f50
pearlsm20 <pearlsm24@gmail.com>
2018-08-06 18:24:13 +0900

    헬로월드

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+		public static void main(String[] args) {
+			System.out.println("Helloworld~!"); 
+			System.out.println("ぞ2ぞ2ぞ2ぞ2ぞ2ぞ2ぞ2ぞ2");
+			System.out.println("噌形");
+		}
+}
